1c58002adb2af2da6dfb1d06695c1dfd670b4a43
EnterOrion <orionwesson@gmail.com>
2023-01-30 18:34:44 +0100

    Minor fixes

--- src/functions/home.js
+++ src/functions/home.js
@@ -2,7 +2,7 @@ function homePage() {
     const inbox = document.getElementById("inbox");
     inbox.innerHTML = " ";
     const inboxPara = document.createElement("p");
-    inboxPara.innerText = "Projects";
+    inboxPara.innerText = "Welcome to your to-do list! To get started, add a task or a project from the sidebar!";
     inbox.appendChild(inboxPara);
 }
 
